0abd461f5820f0388431cd70d38b28aa3b234bdc
Awodire babajide samuel <rhymezx.code@gmail.com>
2023-06-11 18:56:41 +0100

    feat: implemented splash screen

--- app/src/main/java/io/parkinglots/android/MainActivity.kt
+++ app/src/main/java/io/parkinglots/android/MainActivity.kt
@@ -10,11 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.parkinglots.android.ui.theme.ParklotsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+
         setContent {
             ParklotsTheme {
                 // A surface container using the 'background' color from the theme
